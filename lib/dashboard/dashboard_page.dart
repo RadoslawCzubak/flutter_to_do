b36b903a6e0ff5bacec0088b7bf567011361d506
Radoslaw Czubak <radoslaw.marek.czubak@gmail.com>
2021-09-19 17:24:16 +0200

    add main list with title and to do list

--- lib/dashboard/dashboard_page.dart
+++ lib/dashboard/dashboard_page.dart
@@ -1,48 +1,176 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo_app/styles/colors.dart';
 
-class DashboardPage extends StatefulWidget {
+class DashboardPage extends StatelessWidget {
   const DashboardPage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<DashboardPage> createState() => _DashboardPageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+            DashboardUserProfile(),
+            MainTitle(),
+            MainTasksList(),
+            Container(
+              padding: EdgeInsets.only(top: 16.0, left: 16.0, bottom: 10.0),
+              child: RichText(
+                  text: const TextSpan(children: [
+                TextSpan(
+                    text: "To ",
+                    style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black)),
+                TextSpan(
+                    text: "Do",
+                    style: TextStyle(fontSize: 24.0, color: Colors.black)),
+              ])),
+            ),
+            ToDoList()
+          ])),
+    );
+  }
 }
 
-class _DashboardPageState extends State<DashboardPage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class DashboardUserProfile extends StatelessWidget {
+  const DashboardUserProfile({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+    return Container(
+      padding: const EdgeInsets.fromLTRB(16.0, 4.0, 16.0, 8.0),
+      child: Row(
+        children: [
+          ClipOval(
+            child: Image.asset(
+              'assets/images/Profile_avatar_placeholder_large.png',
+              height: 50.0,
+              width: 50.0,
+            ),
+          ),
+          const SizedBox(
+            width: 6.0,
+          ),
+          const Text(
+            'Hello, ',
+            style: TextStyle(fontSize: 20.0),
+          ),
+          const Text(
+            'Amanda',
+            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+          )
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    );
+  }
+}
+
+class MainTitle extends StatelessWidget {
+  const MainTitle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 4.0),
+      child: RichText(
+          text: const TextSpan(children: [
+        TextSpan(
+            text: "4 new ",
+            style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.black)),
+        TextSpan(
+            text: "tasks today",
+            style: TextStyle(fontSize: 24.0, color: Colors.black)),
+      ])),
+    );
+  }
+}
+
+class MainTasksList extends StatelessWidget {
+  const MainTasksList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300.0,
+      child: ListView(
+        itemExtent: 300.0,
+        scrollDirection: Axis.horizontal,
+        children: [
+          MainItem(color: lightOrange),
+          MainItem(color: lightBlue),
+          MainItem(color: lightGreen),
+          MainItem(color: lightRed)
+        ],
+      ),
+    );
+  }
+}
+
+class MainItem extends StatelessWidget {
+  Color color;
+
+  MainItem({
+    Key? key,
+    required this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      child: Container(
+        decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.all(Radius.circular(20.0))),
+      ),
+    );
+  }
+}
+
+class ToDoList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200.0,
+      child: Expanded(
+        child: ListView.separated(
+          padding: EdgeInsets.all(16.0),
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return ToDoItem(lightBlue);
+          },
+          separatorBuilder: (context, index) {
+            return SizedBox(
+              width: 5.0,
+            );
+          },
+          itemCount: 6,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class ToDoItem extends StatelessWidget {
+  Color color;
+  ToDoItem(this.color, {Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(
+        decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.all(Radius.circular(20.0))),
+      ),
     );
   }
 }
---
+++ lib/styles/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const lightOrange = Color.fromARGB(255, 255, 230, 205);
+
+const lightBlue = Color.fromARGB(255, 193, 220, 255);
+
+const lightGreen = Color.fromARGB(255, 233, 244, 245);
+
+const lightRed = Color.fromARGB(255, 252, 237, 234);
+
+const orange = Color.fromARGB(255, 151, 49, 255);
+
+const red = Color.fromARGB(255, 255, 21, 23);
+
+const purple = Color.fromARGB(255, 135, 105, 255);
